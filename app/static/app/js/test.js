6ab8380da37aa9fbcc141c7366353146f6e3b6ea
levanthanhlong <thanhtdbn132001@gmail.com>
2023-07-30 15:50:10 +0700

    30/7

--- app/static/app/js/test.js
+++
@@ -1,6 +0,0 @@
-if (typeof window.location.reload === 'function') {
-    console.log('Trình duyệt của bạn hỗ trợ location.reload()');
-  } else {
-    console.log('Trình duyệt của bạn không hỗ trợ location.reload()');
-  }
-  
